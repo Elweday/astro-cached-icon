96dcb1888ed702146eeb35cb49727791e7d4231b
elweday <elweday22@gmail.com>
2023-12-30 16:41:39 +0200

    fixing lockfile

--- src/utils/save.ts
+++ src/utils/save.ts
@@ -1,7 +1,6 @@
 import fs from 'fs';
 import checksum from './checksum';
 
-
 function getIcon(icon: Icon | string): { pack: string; name: string } {
     if (isIcon(icon)) {
         return { pack: icon.pack, name: icon.name }; // Extract properties for consistency
@@ -16,21 +15,28 @@ function isIcon(value: any): value is Icon {
     return typeof value === 'object' && value !== null && 'pack' in value && 'name' in value; // Check for required properties
 }
 
-async function fetchIcon(icon: Icon | ShortHand): Promise<string> {
+async function fetchIcon(icon: Icon | ShortHand): Promise<string | void> {
     const { pack, name } = getIcon(icon)
     const url = `https://api.iconify.design/${pack}/${name}.svg`;
-    const content = await fetch(url).then(res => res.text());
+    const content = await fetch(url).then(res => res.text()).catch(function () {console.error("Could not find icon", pack,":", name)});
     return content;
 }
+
+
 
 
 export default async function save(icon: Icon | ShortHand) {
     const ic = getIcon(icon)
     const root = "public/cached-icons/";
     const path = root + checksum(ic.pack + ":" + ic.name) + ".svg";
+    if (!fs.existsSync(root)) {
+      fs.mkdirSync(root, { recursive: true });
+    }
+
     if (!fs.existsSync(path)) {
         console.log("fetching", ic.name, "from", ic.pack, "to", path)
         const content = await fetchIcon(ic);
+        if (!content) return;
         fs.writeFileSync(path, content);
         return content
     }
