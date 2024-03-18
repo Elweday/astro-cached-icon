592cb5e826dd743f8432d3dab0ee23baf4dcadd6
elweday <elweday22@gmail.com>
2024-03-18 02:18:57 +0200

    adding to dist

--- src/integration.ts
+++ src/integration.ts
@@ -6,9 +6,9 @@ export function cashed_icons(): AstroIntegration {
         name: "astro-cached-icon",
         hooks: {
             "astro:build:done": async () => {
-                await fs.mkdir("dist/assets/icons", { recursive: true });
+                await fs.mkdir("dist/icons", { recursive: true });
                 const files = await fs.readdir("public/icons");
-                await Promise.all(files.map(async file => fs.copyFile(`public/icons/${file}`, `dist/assets/icons/${file}`)));
+                await Promise.all(files.map(async file => fs.copyFile(`public/icons/${file}`, `dist/icons/${file}`)));
             }
         }
     };
